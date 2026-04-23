c90cdbeab6367ae2ad463371ba5971f7a9d6eede
Pedro Uchôa de Abreu <uchoadev16@gmail.com>
2026-04-23 12:50:41 -0300

    Criando componentes usando tailwind

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-white border-t-2 p-4">
+    <p class="text-center">    
+        Criador por Pedro Uchôa. O codigo fonte está no <a href="#" class="underline">GitHub.</a>
+    </p>
+
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-        {{ config('app.name') }}
-    </title>
-     @vite('resources/css/app.css')
-</head>
-<body>
-     <h1 class="text-3xl font-bold underline text-green-500">
-      Hello world!
-    </h1>
-</body>
-</html>
+<x-layout>
+    <main class="py-10">
+        <h1>
+        Veja seus habitos ganharem vida
+        </h1>
+        
+        <h2>
+
+        </h2> 
+    </main>
+</x-layout>
